2aa5fd89f0ffabe138acd91b43de6a9f21bbaaa7
Clemente Moneta Ruiz <a01174549@tec.mx>
2026-09-07 00:38:00 +0000

    Add team reflection

---
+++ teamreflection.py
@@ -0,0 +1,13 @@
+##Team reflection
+
+
+##We divided the work between the four students,
+## with each student helping with the assigned tasks.
+## Working in separate branches was useful because
+## we could work independently without affecting the main branch.
+
+## From reviewing another student’s code, we learned how to check
+## if the code was correct and if the requirements were completed. 
+##We did not have any major conflicts and we verified the final version 
+##by running both Python files, checking the README, and making sure all the required files were in the main branch.
+
